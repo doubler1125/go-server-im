2f773498004b91f1b92482572b2985f3e9748d06
weipeng.zhang <weipeng.zhang@17zuoye.com>
2023-02-24 11:57:35 +0800

    用户消息广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -64,6 +65,31 @@ func (this *Server) Handler(conn net.Conn) {
 
 	// 广播用户上线消息
 	this.BroadCast(user, "用户上线")
+
+	// 接收客户端消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Printf("Conn read err:", err)
+				return
+			}
+
+			// 提取用户的消息(去除'\n')
+			msg := string(buf[:n-1])
+
+			// 将得到的消息广播
+			this.BroadCast(user, msg)
+		}
+	}()
+
+	// 当前handler阻塞
+	select {}
 }
 
 // 广播消息的方法
